2817abacb18a8e576cb7441c4582c5be4cd0967d
iannjau <lantanacreations@gmail.com>
2024-07-10 16:03:15 +0300

    sd

--- Fit-Mind/src/Components/Mindfullness.jsx
+++ Fit-Mind/src/Components/Mindfullness.jsx
@@ -4,4 +4,9 @@ function ExerciseForm() {
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
     const [difficulty, setDifficulty] = useState('');
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+    }
 }  
