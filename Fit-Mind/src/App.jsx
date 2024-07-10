75c6d0b5594036ef5e6c7e5cdd52a4f18b1811ef
neema <neemabridgetteodire2@gmail.com>
2024-07-10 15:15:21 +0300

    goals-page

--- Fit-Mind/src/App.jsx
+++ Fit-Mind/src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Goal from './Components/GoalsPage'
 
 function App() {
+    return(
+        <>
+        <Goal/>
+        </>
+    )
+
   
 }
 
