de3a998228b3ab2bbc0f3099d7572ad1996d138b
iannjau <lantanacreations@gmail.com>
2024-07-10 19:36:05 +0300

    add

--- Fit-Mind/src/App.jsx
+++ Fit-Mind/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Goal from './Components/GoalsPage'
 import Profile from './Components/Profile'
-import Mindfullness from './Components/Mindfullness'
+import ExerciseForm from './Components/Mindfullness'
 
 function App() {
   return(
     <div className="App">
       <Profile />
      <Goal/>
-     <Mindfullness/>
+     <ExerciseForm/>
     </div>
   )
     
