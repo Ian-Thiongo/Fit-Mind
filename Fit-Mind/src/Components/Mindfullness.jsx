e9831c501ea13775d629a80c5a2dd861d0ad3fdf
iannjau <lantanacreations@gmail.com>
2024-07-10 16:02:08 +0300

    sd

---
+++ Fit-Mind/src/Components/Mindfullness.jsx
@@ -0,0 +1,7 @@
+import React, {useState} from "react";
+
+function ExerciseForm() {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [difficulty, setDifficulty] = useState('');
+}  
