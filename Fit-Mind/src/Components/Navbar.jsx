194d6a47fb3459dfdc038c377143c9033da0dbfc
kaisy <kaisymainam@gmail.com>
2024-07-15 13:34:18 +0300

    adding the sidebar

--- Fit-Mind/src/Components/Navbar.jsx
+++ Fit-Mind/src/Components/Navbar.jsx
@@ -1,28 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './NavBar.css';  
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./NavBar.css";
 
 const NavBar = ({ isLoggedIn, handleLogout }) => {
+  const location = useLocation();
+  const [sidebarVisible, setSidebarVisible] = useState(true);
+  const excludedRoutes = ["/login", "/signup"]; // Add routes where you don't want the sidebar to appear
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+  };
+
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">
-        <Link to="/">FITMIND</Link>
-      </div>
-      <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/goals">Goals</Link></li>
-        <li><Link to="/challenges">Fitness Challenges</Link></li>
-        <li><Link to="/mindfulness">Mindfulness Sessions</Link></li>
-        {isLoggedIn ? (
-          <>
-            <li><Link to="/profile">Profile</Link></li>
-            <li><button className="logout-button" onClick={handleLogout}>Logout</button></li>
-          </>
-        ) : (
-          <li><Link to="/login">Login</Link></li>
-        )}
-      </ul>
-    </nav>
+    <>
+      <button className="toggle-button" onClick={toggleSidebar}>
+        {sidebarVisible ? "Hide Sidebar" : "Show Sidebar"}
+      </button>
+      {(!excludedRoutes.includes(location.pathname) || sidebarVisible) && (
+        <div className="sidebar">
+          <div className="sidebar-brand">
+            <Link to="/" id='fit'>FITMIND</Link>
+          </div>
+          <ul className="sidebar-nav">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/Profile">Profile</Link></li>
+            <li><Link to="/goals">Goals</Link></li>
+            <li><Link to="/challenges">Fitness Challenges</Link></li>
+            <li><Link to="/mindfulness">Mindfulness Sessions</Link></li>
+            <li><Link to="/login">Login</Link></li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 };
 
